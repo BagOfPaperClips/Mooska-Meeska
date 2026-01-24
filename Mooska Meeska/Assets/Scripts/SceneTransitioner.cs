f3ccde4075f2601d52d3914aee115b41b5d7887e
BagOfPaperClips <samrgenovese@gmail.com>
2026-01-24 12:12:55 -0500

    AddedTitleMenuSystem

---
+++ Mooska Meeska/Assets/Scripts/SceneTransitioner.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneTransitioner : MonoBehaviour
+{
+    public void Title()
+    {
+        SceneManager.LoadScene("Title");
+    }
+    public void Game()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void Help()
+    {
+        SceneManager.LoadScene("Help");
+    }
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
