e54848c645ba0084d3ceee5196ac8a1b21a37114
MorpheusTheOne-Cookie <Chigozieaduru@gmail.com>
2026-01-24 16:56:05 -0500

    Small Changed on BirdSO, and settings

--- Mooska Meeska/Assets/Scripts/SO scripts/BirdSO.cs
+++ Mooska Meeska/Assets/Scripts/SO scripts/BirdSO.cs
@@ -6,12 +6,20 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Scriptable Objects/BirdSO")]
 public class BirdSO : ScriptableObject
 {
-   
+
+   [Header("Identification")]
+   public int id;
+
+   [Header("Bird Log")]
    public string birdName;
    public string birdDescription;
    public int birdSpeed;
+
+   [Header ("Audio")]
    public AudioClip birdCall;
    public Sprite birdImage;
+
+   [Header("Reference")]
    public GameObject birdModel;
 
 
