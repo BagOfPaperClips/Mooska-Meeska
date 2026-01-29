a4f033ab7f9558dce75e3ff7ff14f336b6abafd2
allenar7 <allenarciaga7@gmail.com>
2026-01-29 13:44:54 -0500

    moved things around, started working on the keys

--- Mooska Meeska/Assets/Scripts/SO scripts/KeySO.cs
+++ Mooska Meeska/Assets/Scripts/SO scripts/KeySO.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-[CreateAssetMenu(menuName = "Scriptable Objects/BirdSO")]
+[CreateAssetMenu(menuName = "Scriptable Objects/KeySO")]
 
 public class KeySO : ScriptableObject
 {
-    
+    public string color;
+    public Sprite icon;
+    public GameObject key;
 }
