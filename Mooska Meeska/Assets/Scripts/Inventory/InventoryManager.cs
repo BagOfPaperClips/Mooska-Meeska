6d0af2b3adb25db14899c0e925604144c3b01b64
allenar7 <allenarciaga7@gmail.com>
2026-02-01 14:56:49 -0500

    added the addition of keys

--- Mooska Meeska/Assets/Scripts/Inventory/InventoryManager.cs
+++ Mooska Meeska/Assets/Scripts/Inventory/InventoryManager.cs
@@ -1,26 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.ExceptionServices;
+using Cinemachine;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
 {
     public InventorySlot[] inventorySlots;
     public GameObject keyPrefab;
+
+    [Header("Inventory")]
+    public int redKey = 0;
+    public int greenKey = 0;
+    public int yellowKey = 0;
+    public int blueKey = 0;
+
     
     public void AddKey(KeySO key)
     {
-        Debug.Log("Hark the herald");
         
         for(int i = 0; i < inventorySlots.Length; i++)
         {
             
-            Debug.Log("Angel Sing");
             InventorySlot slot = inventorySlots[i];
             KeyItem itemInSlot = slot.GetComponentInChildren<KeyItem>();
             if(itemInSlot == null)
             {
                 
-                Debug.Log("Haleluiah");
                 SpawnNewKey(key, slot);
                 return;
             }
@@ -32,5 +39,27 @@ public class InventoryManager : MonoBehaviour
         GameObject newKeyObject = Instantiate(keyPrefab, slot.transform);
         KeyItem keyItem = newKeyObject.GetComponent<KeyItem>();
         keyItem.InitializeKey(key);
+
+        string colour = key.color;
+
+        switch (colour)
+        {
+            case("Red"):
+            redKey++;
+            break;
+            case("Blue"):
+            blueKey++;
+            break;
+            case("Yellow"):
+            yellowKey++;
+            break;
+            case("Green"):
+            greenKey++;
+            break;
+
+
+        }
+
+        Debug.Log("you have aquired the "+colour+" key");
     }
 }
