e6a4c113ab20921fce75351ad3c780b221531d25
weimst <erim.lee.01@gmail.com>
2026-02-02 14:11:38 -0500

    Instructions

---
+++ Mooska Meeska/Assets/Scripts/Management/Instructions.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Instructions : MonoBehaviour
+{
+
+    string[] pageText = 
+    {
+        "", "Avoid the birds flying overhead! If they spot you, " + 
+        "you won't have much time to find cover..", "Interact with " +
+        "wanted posters (cubes atm) to learn more about the bird mafia",
+        "Find keys to unlock spaces and save Meeska!"
+    };
+    public int pageindex = 0;
+    public TextMeshProUGUI bodyText;
+    public TextMeshProUGUI nextBtn;
+    public GameObject instructionPanel;
+
+
+    // Start is called before the first frame update
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return)) {
+            nextPage();
+        }
+    }
+    public void nextPage() {
+        if (pageindex == pageText.Length - 1) {
+            instructionPanel.SetActive(false);
+        }
+        else {
+            pageindex++;
+            showPage();
+            if (pageindex == pageText.Length - 1) {
+                nextBtn.text = "Start Game";
+            }
+         }
+    }
+
+    public void showPage() {
+        bodyText.text = pageText[pageindex];
+    }
+}
