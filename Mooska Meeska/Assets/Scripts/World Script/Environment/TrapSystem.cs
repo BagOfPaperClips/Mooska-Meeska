3bc824d9fcaa8da0f539c7c0ec64138ec890b98d
allenar7 <allenarciaga7@gmail.com>
2026-02-02 21:21:21 -0500

    finished 1 trap

--- Mooska Meeska/Assets/Scripts/World Script/Environment/TrapSystem.cs
+++ Mooska Meeska/Assets/Scripts/World Script/Environment/TrapSystem.cs
@@ -6,6 +6,7 @@ public class TrapSystem : MonoBehaviour
 {
     
     public Interactables[] traps;
+    public List<Interactables> activatedTraps = new List<Interactables>();
     
     
     void Awake()
@@ -20,22 +21,36 @@ public class TrapSystem : MonoBehaviour
         while (true)
         {
             yield return new WaitForSeconds(5);
+            
+            activatedTraps.Clear();
 
 
 
+            //Disables the current traps
             for(int i = 0; i < traps.Length; i++)
             {
-                
                 traps[i].gameObject.SetActive(false);
                 traps[i].alert.gameObject.SetActive(false);
+            }
 
-                if(Random.Range(0,2) == 0)
+            //sets the alerts and determines which traps will be turned on
+            for(int i = 0; i < traps.Length; i++)
+            {
+                if (Random.Range(0, 2) == 0)
                 {
-                    traps[i].alert.gameObject.SetActive(true);
-                    yield return new WaitForSeconds(2);
-                    traps[i].gameObject.SetActive(true);
                     
+                    traps[i].alert.gameObject.SetActive(true);
+                    activatedTraps.Add(traps[i]);
                 }
+            }
+
+            // wait for seconds
+            yield return new WaitForSeconds(2);
+
+            // activate the traps
+            for(int i = 0; i < activatedTraps.Count; i++)
+            {
+                activatedTraps[i].gameObject.SetActive(true);
             }
 
         }
