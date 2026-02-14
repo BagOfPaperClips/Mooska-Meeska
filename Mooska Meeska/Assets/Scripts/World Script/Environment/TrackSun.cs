2afcb73bcb7378978308e4211233311b50760bb9
allenar7 <allenarciaga7@gmail.com>
2026-02-13 20:27:17 -0500

    hopefully lighting works

--- Mooska Meeska/Assets/Scripts/World Script/Environment/TrackSun.cs
+++ Mooska Meeska/Assets/Scripts/World Script/Environment/TrackSun.cs
@@ -5,13 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class TrackSun : MonoBehaviour
 {
-    public Transform sun;
+
+
+    public Transform sun;          // I lasso the sun
     public Transform player;
+
     public LayerMask wall;
     public LayerMask bird;
+
     public bool hidden;
+
     public static TrackSun instance;
+
     private MouseLook mouseLook;
+    private LayerMask detectionMask;
 
     void Awake()
     {
@@ -24,38 +31,107 @@ public class TrackSun : MonoBehaviour
         instance = this;
 
         mouseLook = FindFirstObjectByType<MouseLook>();
+        detectionMask = wall | bird;
     }
-    // Start is called before the first frame update
+
     void Update()
     {
-        // if(Physics.Linecast(sun.position, player.position, wall))
-        // {
-        //     //PLAYER IN SHADOW
+        Vector3 sunDirection = -sun.forward; 
 
-        //     Debug.Log("IN SHADOW");
-        //     hidden = true;
+        Debug.DrawRay(player.position, sunDirection * 100f, Color.yellow);
 
-        // }
-        hidden = Physics.Linecast(sun.position, player.position, wall);
+        RaycastHit hit;
+
+        if (Physics.Raycast(player.position, sunDirection, out hit, 1000f, detectionMask))
+        {
+            
+
+            if (((1 << hit.collider.gameObject.layer) & wall) != 0)
+            {
+                hidden = true;
+                return;
+            }
+
+            if (((1 << hit.collider.gameObject.layer) & bird) != 0)
+            {
+                hidden = false;
+
+                Debug.Log("CAUGHT");
+
+                BirdInstance birdInstance = hit.collider.GetComponentInParent<BirdInstance>();
+
+                if (birdInstance != null)
+                {
+                    Debug.Log("Who's that pokemon? ITS " + birdInstance.data.birdName);
+                }
+
+                mouseLook.UnlockCursor();
+                SceneManager.LoadScene("Death");
+            }
+        }
+        else
+        {
+            // Nothing blocking sunlight
+            hidden = false;
+        }
+    }
+
+
+
+    // public Transform sun;
+    // public Transform player;
+    // public LayerMask wall;
+    // public LayerMask bird;
+    // public bool hidden;
+    // public static TrackSun instance;
+    // private MouseLook mouseLook;
+
+    // void Awake()
+    // {
+    //     if (instance != null && instance != this)
+    //     {
+    //         Destroy(gameObject);
+    //         return;
+    //     }
+
+    //     instance = this;
+
+    //     mouseLook = FindFirstObjectByType<MouseLook>();
+    // }
+    // // Start is called before the first frame update
+    // void Update()
+    // {
+    //     // if(Physics.Linecast(sun.position, player.position, wall))
+    //     // {
+    //     //     //PLAYER IN SHADOW
+
+    //     //     Debug.Log("IN SHADOW");
+    //     //     hidden = true;
+
+    //     // }
+        
+    //     Vector3 sunDirection = -sun.forward;
+        
+    //     hidden = Physics.Linecast(sun.position, player.position, wall);
 
     
-        RaycastHit hit;
-        if(Physics.Linecast(sun.position, player.position, out hit, bird) && !hidden)
-        {
+    //     RaycastHit hit;
+    //     if(Physics.Linecast(sun.position, player.position, out hit, bird) && !hidden)
+    //     {
 
-            Debug.Log("CAUGHT");
-            //hostile == true;
-            //find the bird that caught it, go to individual instance (does that work when its not an instance.? and make dive methoid{})
-            BirdInstance bird = hit.collider.GetComponentInParent<BirdInstance>();
-            Debug.Log("Who's that pokemon? ITS " + bird.data.birdName);
-            mouseLook.UnlockCursor();
-            //bird.diveStart();
-            SceneManager.LoadScene("Death");
+    //         Debug.Log("CAUGHT");
+    //         //hostile == true;
+    //         //find the bird that caught it, go to individual instance (does that work when its not an instance.? and make dive methoid{})
+    //         BirdInstance bird = hit.collider.GetComponentInParent<BirdInstance>();
+    //         Debug.Log("Who's that pokemon? ITS " + bird.data.birdName);
+    //         mouseLook.UnlockCursor();
+    //         //bird.diveStart();
+    //         SceneManager.LoadScene("Death");
 
-        }
+    //     }
 
-        //PLAYER IN SUN
-    }
+    //     //PLAYER IN SUN
+    // }
 
 
 }
