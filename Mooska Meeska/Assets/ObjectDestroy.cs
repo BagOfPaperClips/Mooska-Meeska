a2625681957a77c80bb17efd4b2a906c95511d62
BagOfPaperClips <samrgenovese@gmail.com>
2026-03-09 23:30:50 -0400

    Puzzle1 Done

---
+++ Mooska Meeska/Assets/ObjectDestroy.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDestroy : MonoBehaviour
+{
+
+    [SerializeField] BoxCollider box;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            box = gameObject.GetComponent<BoxCollider>();
+            box.enabled = false;
+            Destroy(gameObject, 0.8f);
+        }
+    }
+
+}
