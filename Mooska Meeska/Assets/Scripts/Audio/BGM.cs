18aed46515e9caf337f21731154698da08558faf
SAT362 <spidersathu@gmail.com>
2026-03-18 00:31:05 -0400

    birds sounds

---
+++ Mooska Meeska/Assets/Scripts/Audio/BGM.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BGM : MonoBehaviour
+{
+    public AudioSource gameSong;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameSong = gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameSong.enabled =
+            SceneManager.GetActiveScene().name.Equals("Credits") ||
+            SceneManager.GetActiveScene().name.Equals("Help") ||
+            SceneManager.GetActiveScene().name.Equals("Title");
+    }
+}
