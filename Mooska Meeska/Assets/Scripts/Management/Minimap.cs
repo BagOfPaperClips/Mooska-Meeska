353923236a8e88f271be4cf8a89cdb3aa7f00720
allenar7 <allenarciaga7@gmail.com>
2026-03-20 22:33:50 -0400

    Effectively done with the minimap system

--- Mooska Meeska/Assets/Scripts/Management/Minimap.cs
+++ Mooska Meeska/Assets/Scripts/Management/Minimap.cs
@@ -6,18 +6,7 @@ using UnityEngine;
 public class Minimap : MonoBehaviour
 {
     
-    public Transform player;
-
-
-    void LateUpdate()
-    {
-        Vector3 newPosition = player.position;
-        newPosition.y = transform.position.y;
-        transform.position = newPosition;
-
-        //transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
-
-    }
-
+    public Camera minimapCamera;
+    public RenderTexture RenTex;
 
 }
